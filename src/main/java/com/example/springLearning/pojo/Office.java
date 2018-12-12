f7025d40cd7fc154b3f8082143dd4bfaa26d0a76
fly <1779905848@qq.com>
2018-12-12 13:51:36 +0800

    工作室实体类 -- fly

---
+++ src/main/java/com/example/springLearning/pojo/Office.java
@@ -0,0 +1,40 @@
+package com.example.springLearning.pojo;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * @author  樊立扬
+ **/
+@Entity
+@Table
+@Data
+public class Office {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column
+    private String name;
+    @Column(nullable = false)// 学段外键
+    private Integer sectionId;
+    @Column(nullable = false) // 学科外键
+    private Integer subject;
+    @Column // 省
+    private String state;
+    @Column // 市
+    private String city;
+    @Column // 区县
+    private String area;
+    @Column // 关注人数
+    private Integer follows;
+    @Column // 文章数量
+    private Integer article;
+    @Column // 成果数量
+    private Integer achievements;
+    @Column // 成员人数
+    private Integer members;
+
+
+}
